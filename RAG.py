a89ad0a51fbca8cb3c5952180a460defc31e1a18
Trung-Le914 <trung149hn@gmail.com>
2025-07-06 22:46:50 +0700

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -54,7 +54,8 @@ def load_embeddings():
 @st.cache_resource  
 def load_llm():
     #MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    MODEL_NAME = "Qwen/Qwen1.5-0.5B-Chat"
+    #MODEL_NAME = "Qwen/Qwen1.5-0.5B-Chat"
+    MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 
     #bnb_config = BitsAndBytesConfig(
     #    load_in_4bit=True,  # Hoặc load_in_8bit=True
